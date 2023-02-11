2d87ded657432bf7f91252af422ca9d85d86a2f1
admin <admin@localhost>
2023-02-11 16:53:06 +0300

    refactor: showDialog (front)

--- static/js/ui.js
+++ static/js/ui.js
@@ -2,3 +2,14 @@ export const showMessage = (msg) => {
     // TODO: rewrite
     alert(msg);
 };
+
+export const showDialog = (htmlContent, callback) => {
+    const dialog = document.getElementById('dialog');
+    dialog.style.display = 'flex';
+    dialog.innerHTML = htmlContent;
+    dialog.addEventListener('click', (e) => {
+        callback(e);
+        //dialog.style.display = 'none';
+    }, true);
+    
+};
